8e02a556da75e5c92ec1c1c12b8485d5402e9022
JuanGarces10 <juan.garcesmoreno@unicolombo.edu.co>
2024-05-16 22:59:08 -0500

    Cambios

---
+++ ProyectoA/src/guiA/datos/Usuario.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guiA.datos;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Usuario {
+    public static String id;
+    public static String clave;
+    public static String nombre;
+    public static String genero;
+    public static String email;
+     public static HashMap<String, Usuario> usuariosNuevos;
+    
+}
