3e2b3ae87e5c11a8bf606a926a12d7cec551aa10
FaberVillarreal18 <faber.villarrealantequera@unicolombo.edu.co>
2024-05-18 23:05:00 -0500

    Arreglo Faber

--- ProyectoA/src/guiA/datos/Usuario.java
+++ ProyectoA/src/guiA/datos/Usuario.java
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package guiA.datos;
-
-import java.util.HashMap;
-
-/**
- *
- * @author LENOVO
- */
-public class Usuario {
-    public static String id ;
-    public static String clave ;
-    public static String nombre;
-    public static String genero;
-    public static String email;
-     public static HashMap<String, Usuario> usuariosNuevos;
-     
-     
-     
-     public String getid(){
-         return id;
-     }
-     
-     public  String getclave(){
-         return clave;
-     }
-    
-}
-
-=======
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -64,4 +30,3 @@ public class Usuario {
     
 }
 
->>>>>>> origin/master
--- ProyectoA/src/proyectoa/ProyectoA.java
+++ ProyectoA/src/proyectoa/ProyectoA.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -28,34 +27,3 @@ public class ProyectoA {
     
     
 }
-=======
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
-package proyectoa;
-
-import guiA.VentanaFacturas;
-import guiA.VentanaPrincipal;
-
-/**
- *
- * @author Estudiante
- */
-public class ProyectoA {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        VentanaPrincipal VentanaL = new VentanaPrincipal();
-        VentanaL.setVisible(true);
-        VentanaL.setLocationRelativeTo(null);
-       
-    }
-   
-    
-    
-    
-}
->>>>>>> origin/master
