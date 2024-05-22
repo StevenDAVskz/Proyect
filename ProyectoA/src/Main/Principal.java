e9287e2f3eb50d5cf0f70d517ea5b8ea3091dcce
JuanGarces10 <juan.garcesmoreno@unicolombo.edu.co>
2024-05-21 21:21:40 -0500

    Crear factura bd

---
+++ ProyectoA/src/Main/Principal.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+import guiA.VentanaPrincipal;
+
+/**
+ *
+ * @author juang
+ */
+public class Principal {
+    
+    
+        public static void main(String[] args) {
+            VentanaPrincipal main = new  VentanaPrincipal();
+            main.setVisible(true);
+            main.setLocationRelativeTo(null);
+    }
+    
+}
---
+++ ProyectoA/src/guiA/datos/Usuario.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package guiA.datos;
+
+
+
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Usuario {
+    public static String id ;
+    public static String clave ;
+    public static String nombre;
+    public static String genero;
+    
+    
+}
+
